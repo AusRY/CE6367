298b8355ac4e5e1c6a3874295cc95c72c414537a
AusRY <ary091020@utdallas.edu>
2016-04-18 11:44:49 -0500

    Added files via upload

---
+++ src/Helloworld.java
@@ -0,0 +1,23 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asedf");
+<<<<<<< HEAD
+		
+		Tester asdf = new Tester();
+		
+		System.out.println(asdf.addkj());
+		
+		asdf.setk();
+		
+		System.out.println(asdf.addkj());
+		
+		
+=======
+
+>>>>>>> refs/heads/master
+	}
+
+}
---
+++ src/Tester.java
@@ -0,0 +1,42 @@
+
+public class Tester {
+	
+	
+	private int j;
+	private int k;
+	private int s;
+	
+	public Tester()
+	{
+		
+		j = 1;
+		k = 2;
+		
+	}
+	
+	
+	
+	public void setj()
+	{
+		j = 3;
+		
+		
+	}
+	
+	public void setk()
+	{
+		k = 5;
+		
+		
+	}
+	
+	public int addkj()
+	{
+		s = k + j;
+		
+		return s;
+		
+	}
+	
+	
+}
