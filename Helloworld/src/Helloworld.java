21653307b3f7c5f75e5ec5016722bf9c09235ff3
Aus <Aus@KallV3>
2016-04-18 10:47:39 -0500

    New

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -4,7 +4,16 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("asedf");
-
+		
+		Tester asdf = new Tester();
+		
+		System.out.println(asdf.addkj());
+		
+		asdf.setk();
+		
+		System.out.println(asdf.addkj());
+		
+		
 	}
 
 }
